83a0a684f9c41a39c8739d2ca616637672769b9f
StevenSriram <sriram242004@gmail.com>
2025-03-06 23:38:45 +0530

    Starter Project

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { IconCloud } from "@/components/magicui/icon-cloud";
+import ParticlesDemo from "@/components/custom/ParticlesDemo";
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "dart",
+  "java",
+  "react",
+  "flutter",
+  "android",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "amazonaws",
+  "postgresql",
+  "firebase",
+  "nginx",
+  "vercel",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "sonarqube",
+  "figma",
+];
+
+export default function App() {
+  const images = slugs.map(
+    (slug) => `https://cdn.simpleicons.org/${slug}/${slug}`
+  );
+
+  return (
+    <>
+      <div className="relative flex size-full items-center justify-center overflow-hidden">
+        <IconCloud images={images} />
+      </div>
+      <div className="relative flex size-full items-center justify-center overflow-hidden">
+        <ParticlesDemo />
+      </div>
+    </>
+  );
+}
